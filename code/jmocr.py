531e9c665b134afdcdbb95075bac8ec4b28489eb
Justin Matters <justinmatters@gmail.com>
2021-09-22 13:40:26 +0100

    Setup basic plan

---
+++ code/jmocr.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+import sys
+
+## Broad plan overview
+
+# read arguments
+
+# open file
+
+# greyscale
+
+# blur
+
+# threshold
+
+# get contour
+
+# apply transformation too original image
+
+# greyscale rectified image
+
+# denoise
+
+# blur rectified image
+
+# threshhold rectified image
+
+# text detection (fast and avoids trying to detect non existent text)
+
+# get contours
+
+# pass to tesseract for OCR
+
+# output OCRed text 
