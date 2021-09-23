b95e0c5256b227c331b161688432a4b17b96d275
Justin Matters <justinmatters@gmail.com>
2021-09-23 21:10:05 +0100

    Add preprocessing
    Add preprocessing. Also lint and run black formatter to improve readability

--- ocrcode/ocr.py
+++ ocrcode/ocr.py
@@ -3,7 +3,8 @@ import numpy as np
 import os
 import sys
 
-def get_paths(*file_names : str):
+
+def get_paths(*file_names: str):
     """given a file string or list of file strings, returns a list of full paths
     to images
 
@@ -20,13 +21,14 @@ def get_paths(*file_names : str):
             full_path = os.path.abspath(file_name)
             files_list.append(full_path)
         return files_list
-    else: 
+    else:
         # TBD for now default to returning our test image
-        file_name = 'data/business card.jpg'
+        file_name = "data/business card.jpg"
         full_path = os.path.abspath(file_name)
         return [full_path]
 
-def scale_longest_axis(image: np.ndarray, new_size = 512):
+
+def scale_longest_axis(image: np.ndarray, new_size=512):
     """resize an image so its longest axis (height or width) equals the 
     specified new size. Choose appropriate interpolation depending on whether 
     we are growing or shrinking
@@ -44,21 +46,22 @@ def scale_longest_axis(image: np.ndarray, new_size = 512):
     # find long axis then calculate new shape
     if width > height:
         # weirldy though image.shape is h*w*d resize is w*h !!
-        new_dimensions = (new_size, int(height*new_size//width))
+        new_dimensions = (new_size, int(height * new_size // width))
     else:
         # weirldy though image.shape is h*w*d resize is w*h !!
-        new_dimensions = (int(width*new_size//height), new_size)
+        new_dimensions = (int(width * new_size // height), new_size)
     print(new_dimensions)
     # are we growing or shrinking - we need appropriate interpolation
     if width > new_size or height > new_size:
-        # shrinking 
+        # shrinking
         interpolation = cv2.INTER_AREA
     else:
         # growing
         interpolation = cv2.INTER_CUBIC
     # rescale
-    return cv2.resize(image, new_dimensions, interpolation = interpolation)
+    return cv2.resize(image, new_dimensions, interpolation=interpolation)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # we have no cause to run this file directly currently
     pass
--- test/ocr_test.py
+++ test/ocr_test.py
@@ -3,33 +3,31 @@ from ocrcode import ocr
 import cv2
 import numpy as np
 
+
 class TestScaleLongestAxis:
     """Test Class for ocr.scale_longest_axis"""
 
     @pytest.mark.parametrize(
         # remember numpy arrays are h*w*d or y*x*d
-        'y_in, x_in, scale, y_out, x_out',
+        "y_in, x_in, scale, y_out, x_out",
         [
             # scale down
-            (1024,1024,512,512,512),
-            #scale up
-            (10,10,512,512,512),
+            (1024, 1024, 512, 512, 512),
+            # scale up
+            (10, 10, 512, 512, 512),
             # scale on non default scale
-            (10,10,40,40,40),
+            (10, 10, 40, 40, 40),
             # scale on x
-            (1200,600,512,512,256),
+            (1200, 600, 512, 512, 256),
             # scale on y
-            (300,600,512,256,512),
-        ]
+            (300, 600, 512, 256, 512),
+        ],
     )
     def test_correct_scaling(self, x_in, y_in, scale, x_out, y_out):
         # create a pseudo input image
-        image_in = np.ones((x_in,y_in,3))
+        image_in = np.ones((x_in, y_in, 3))
         # create the expected output image
-        expected_out = np.ones((x_out,y_out,3))
-        function_out = ocr.scale_longest_axis(
-            image = image_in, 
-            new_size = scale
-        )
+        expected_out = np.ones((x_out, y_out, 3))
+        function_out = ocr.scale_longest_axis(image=image_in, new_size=scale)
         # check the output shapes are correct
-        assert expected_out.shape  == function_out.shape
+        assert expected_out.shape == function_out.shape
